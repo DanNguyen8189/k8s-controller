3d8cf0a0d7d511f6e57fcb18b0611b7a0c7619d2
dannguyen8189 <dannguyen8189@gmail.com>
2026-03-03 10:33:14 -0800

    Added basic montioring of pods in a namespace. Track statuses in pod/container lifecycle

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 // https://oneuptime.com/blog/post/2026-01-07-go-client-go-kubernetes/view
 
 import (
-	//"context"
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 )
 
 func main() {
@@ -40,18 +42,125 @@ func main() {
 
 	fmt.Printf("Connected to Kubernetes %s\n", version.GitVersion)
 
-	pods, err := getPods(clientset, "kong")
-	if err != nil {
-		fmt.Printf("Error getting pods: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("Getting pods in kong namespace")
-	for _, pod := range pods.Items {
-		fmt.Printf("Pod: %s\n", pod.Name)
-	}
+	fmt.Printf("Monitoring pods in kong namespace\n")
+	monitorPods(clientset, "kong")
 
 	// Keep the process alive by sleeping indefinitely
 	select {}
+}
+
+// monitorPods watches pods in a namespace and detects status changes
+func monitorPods(clientset *kubernetes.Clientset, namespace string) {
+
+	// Use k8s informers to continuously monitor pods. Resyncs every 30s
+
+	factory := informers.NewSharedInformerFactoryWithOptions(
+		clientset,
+		30*time.Second,
+		informers.WithNamespace(namespace),
+	)
+
+	podInformer := factory.Core().V1().Pods().Informer()
+
+	podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		// log new pods and check status
+		AddFunc: func(obj interface{}) {
+			pod := obj.(*v1.Pod)
+			fmt.Printf("[ADD] Pod: %s/%s\n", pod.Namespace, pod.Name)
+			checkPodStatus(pod)
+		},
+		// log status changes
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			newPod := newObj.(*v1.Pod)
+			oldPod := oldObj.(*v1.Pod)
+
+			// Only process if status changed
+			if oldPod.Status.Phase != newPod.Status.Phase ||
+				hasContainerStatusChanged(oldPod.Status.ContainerStatuses, newPod.Status.ContainerStatuses) {
+				fmt.Printf("[UPDATE] Pod: %s/%s - Phase: %s\n", newPod.Namespace, newPod.Name, newPod.Status.Phase)
+				checkPodStatus(newPod)
+			}
+		},
+		// log pod deletions
+		DeleteFunc: func(obj interface{}) {
+			pod := obj.(*v1.Pod)
+			fmt.Printf("[DELETE] Pod: %s/%s\n", pod.Namespace, pod.Name)
+		},
+	})
+
+	stop := make(chan struct{})
+	factory.Start(stop)
+	factory.WaitForCacheSync(stop)
+}
+
+// checkPodStatus examines a pod and reports any issues
+func checkPodStatus(pod *v1.Pod) {
+	// Check overall pod phase
+	switch pod.Status.Phase {
+	case v1.PodPending:
+		fmt.Printf("  ⏳ Pod is Pending\n")
+	case v1.PodRunning:
+		fmt.Printf("  ✓ Pod is Running\n")
+	case v1.PodSucceeded:
+		fmt.Printf("  ✓ Pod Succeeded\n")
+	case v1.PodFailed:
+		fmt.Printf("  ✗ Pod Failed - Reason: %s\n", pod.Status.Reason)
+	case v1.PodUnknown:
+		fmt.Printf("  ? Pod Unknown\n")
+	}
+
+	// Check each container status for issues
+	for i, containerStatus := range pod.Status.ContainerStatuses {
+		if isCrashLoop(containerStatus) {
+			fmt.Printf("  ⚠️  Container %d (%s) is in CrashLoopBackOff (Restarts: %d)\n",
+				i, containerStatus.Name, containerStatus.RestartCount)
+		} else if isOOMKilled(containerStatus) {
+			fmt.Printf("  💥 Container %d (%s) was OOMKilled\n", i, containerStatus.Name)
+		} else if containerStatus.State.Waiting != nil {
+			fmt.Printf("  ⏳ Container %d (%s) is Waiting - Reason: %s\n",
+				i, containerStatus.Name, containerStatus.State.Waiting.Reason)
+		} else if containerStatus.State.Terminated != nil {
+			fmt.Printf("  ⏹️  Container %d (%s) is Terminated - Reason: %s (Exit Code: %d)\n",
+				i, containerStatus.Name, containerStatus.State.Terminated.Reason,
+				containerStatus.State.Terminated.ExitCode)
+		}
+	}
+}
+
+// hasContainerStatusChanged checks if any container status changed. Prevents duplicate alerts
+func hasContainerStatusChanged(oldStatuses, newStatuses []v1.ContainerStatus) bool {
+	if len(oldStatuses) != len(newStatuses) {
+		return true
+	}
+
+	for i, newStatus := range newStatuses {
+		if i >= len(oldStatuses) {
+			return true
+		}
+		oldStatus := oldStatuses[i]
+
+		if oldStatus.RestartCount != newStatus.RestartCount {
+			return true
+		}
+
+		// Compare state
+		if (oldStatus.State.Waiting != nil) != (newStatus.State.Waiting != nil) {
+			return true
+		}
+		if (oldStatus.State.Running != nil) != (newStatus.State.Running != nil) {
+			return true
+		}
+		if (oldStatus.State.Terminated != nil) != (newStatus.State.Terminated != nil) {
+			return true
+		}
+
+		// Compare last termination state
+		if (oldStatus.LastTerminationState.Terminated != nil) != (newStatus.LastTerminationState.Terminated != nil) {
+			return true
+		}
+	}
+
+	return false
 }
 
 // Check for specific error types
@@ -73,42 +182,14 @@ func handleError(err error) {
 
 func getPods(client *kubernetes.Clientset, namespace string) (*v1.PodList, error) {
 	pods, err := client.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
-	// if err != nil {
-	// 	//fmt.Printf("Error getting server version: %v\n", err)
-	// 	return pods, err
-	// }
 	return pods, err
 }
 
 func isCrashLoop(cs v1.ContainerStatus) bool {
 	return cs.State.Waiting != nil && cs.State.Waiting.Reason == "CrashLoopBackOff"
 }
+
 func isOOMKilled(cs v1.ContainerStatus) bool {
 	return cs.LastTerminationState.Terminated != nil &&
 		cs.LastTerminationState.Terminated.Reason == "OOMKilled"
 }
-
-// func handlePodUpdate(oldObj, newObj interface{}) {
-// 	pod := newObj.(*v1.Pod)
-// 	for _, cs := range pod.Status.ContainerStatuses {
-// 		var reason string
-// 		switch {
-// 		case isCrashLoop(cs):
-// 			reason = "CrashLoopBackOff"
-// 		case isOOMKilled(cs):
-// 			reason = "OOMKilled"
-// 		default:
-// 			continue
-// 		}
-// 		key := fmt.Sprintf("%s/%s/%s/%s/%d", pod.Namespace, pod.Name, cs.Name, reason, cs.RestartCount)
-// 		if seenRecently(key) {
-// 			continue
-// 		}
-
-// 		sendDiscord(webhookURL, pod, cs, reason)
-
-// 		if shouldRemediate(pod, reason) {
-// 			_ = client.CoreV1().Pods(pod.Namespace).Delete(ctx, pod.Name, metav1.DeleteOptions{})
-// 		}
-// 	}
-// }
